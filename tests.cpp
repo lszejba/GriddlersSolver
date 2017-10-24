4a4d41c462792eb6a9155c347127b1fba0a43957
Łukasz Szejba <lukasz.szejba@gmail.com>
2017-10-24 17:29:03 +0200

    Small refactoring, tests - start

---
+++ tests.cpp
@@ -0,0 +1,18 @@
+#include "tests.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION( GriddlersTest );
+
+void GriddlersTest::setUp()
+{
+
+}
+
+void GriddlersTest::tearDown()
+{
+
+}
+
+void GriddlersTest::testConstructor()
+{
+
+}
