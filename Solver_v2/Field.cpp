789e83b5c191fea11420a08441d787cab78d2be1
Lukasz Szejba <lukasz.szejba@gmail.com>
2025-10-28 01:16:16 +0100

    Field class updated

--- Solver_v2/Field.cpp
+++ Solver_v2/Field.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
 #include <set>
 #include "Field.hpp"
+
+Field::Field(int x, int y) : state(FieldState::Unknown), rowNumber(x), columnNumber(y)
+{
+}
 
 void Field::Register(LogicalGroup& group)
 {
@@ -9,4 +14,41 @@ void Field::Register(LogicalGroup& group)
 void Field::Unregister(LogicalGroup& group)
 {
     registeredGroups.erase(&group);
+}
+
+void Field::UpdateState(FieldState newState)
+{
+    if (state != FieldState::Unknown)
+    {
+        return; // once set, state can't be modified'
+    }
+    if (newState == FieldState::Unknown)
+    {
+        return;
+    }
+
+    state = newState;
+    for (auto logicalGroup : registeredGroups)
+    {
+        logicalGroup->Process();
+    }
+}
+
+void Field::Print()
+{
+    switch(state)
+    {
+        case FieldState::Unknown:
+            std::cout << ".";
+            break;
+        case FieldState::Empty:
+            std::cout << "X";
+            break;
+        case FieldState::Full:
+            std::cout << "#";
+            break;
+        default:
+            std::cout << "?";
+            break;
+    }
 }
--- Solver_v2/Field.hpp
+++ Solver_v2/Field.hpp
@@ -4,15 +4,27 @@
 #include <set>
 #include "LogicalGroup.hpp"
 
+enum FieldState
+{
+    Unknown = -1,
+    Empty = 0,
+    Full = 1
+};
+
 class Field
 {
 public:
     Field(int x, int y);
     void Register(LogicalGroup &);
     void Unregister(LogicalGroup &);
+    void UpdateState(FieldState newState);
+    void Print();
 
 private:
     std::set<LogicalGroup *> registeredGroups;
+    FieldState state;
+    int rowNumber;
+    int columnNumber;
 };
 
 #endif
