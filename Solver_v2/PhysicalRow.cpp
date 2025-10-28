a01af3daac830730d539c8684fbdd1eb373b6a5e
Lukasz Szejba <lukasz.szejba@gmail.com>
2025-10-28 01:16:53 +0100

    PhysicalRow class updated

--- Solver_v2/PhysicalRow.cpp
+++ Solver_v2/PhysicalRow.cpp
@@ -1,1 +1,12 @@
 #include "PhysicalRow.hpp"
+
+PhysicalRow::PhysicalRow(int n, int size) : number(n)
+{
+    contents.reserve(size);
+}
+
+void PhysicalRow::SetField(int n, std::shared_ptr<Field> field)
+{
+    contents[n] = field;
+}
+
--- Solver_v2/PhysicalRow.hpp
+++ Solver_v2/PhysicalRow.hpp
@@ -1,16 +1,19 @@
 #ifndef PHYSICAL_ROW
 #define PHYSICAL_ROW
 
+#include <memory>
 #include <vector>
 #include "Field.hpp"
 
 class PhysicalRow
 {
 public:
+    PhysicalRow(int n, int size);
 
+    void SetField(int n, std::shared_ptr<Field> field);
 private:
     int number;
-    std::vector<Field *> contents;
+    std::vector<std::shared_ptr<Field>> contents;
 };
 
 #endif
