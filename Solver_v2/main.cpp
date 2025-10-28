f8721f6f1787b70d3bc25cb24769880490640a6f
Lukasz Szejba <lukasz.szejba@gmail.com>
2025-10-28 01:20:28 +0100

    Printing board added

--- Solver_v2/main.cpp
+++ Solver_v2/main.cpp
@@ -8,6 +8,13 @@ int main(int argc, char **argv) {
 
     board.LoadNewBoard("tests/5x5_3fullrows.test");
     board.PrintBaseInfo();
+    board.PrintBoardContents();
+
+    std::cout << std::endl << std::endl << std::endl;
+
+    board.LoadNewBoard("tests/5x3_2fullrows.test");
+    board.PrintBaseInfo();
+    board.PrintBoardContents();
 
     return 0;
 }
